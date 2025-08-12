e2da9e953e7e262b6e811788e2f0513e28d41824
lzhang-scitix <lzhang@scitix.ai>
2025-08-12 06:34:25 +0000

    add reason

--- internal/selfhealing/node_sop/gpu/broken.go
+++ internal/selfhealing/node_sop/gpu/broken.go
@@ -46,7 +46,7 @@ func (g *gpubroken) Execute(ctx context.Context, node string, status *prom.Aegis
 	}
 
 	if !g.bridge.TicketManager.CheckTicketExists(ctx) {
-		g.bridge.TicketManager.CreateTicket(ctx, status, basic.HardwareTypeGpu)
+		g.bridge.TicketManager.CreateTicket(ctx, status, basic.HardwareTypeGpu, reason)
 		g.bridge.TicketManager.AddRootCauseDescription(ctx, status.Condition, status)
 		g.bridge.TicketManager.AdoptTicket(ctx)
 		return nil
